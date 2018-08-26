aba36a932c71ca5148781b163cd0fa44a842658e
sewon <kamilog.yomoto@gmail.com>
2018-08-27 05:12:46 +0900

    주석 추가

--- app/src/main/java/com/sewon/android/helloandroid3/MainActivity.java
+++ app/src/main/java/com/sewon/android/helloandroid3/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/*
+ * Hello Android 프로젝트
+ */
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
